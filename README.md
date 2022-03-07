3547c8308c70470fddbb31b0f2ce2f360625867a
hanscal <hanscalcai@163.com>
2022-03-07 15:41:47 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # OverView
 **一个基于Rasa3.0+的中文对话机器人, 一方面支持知识问答，另一方面支持智能闲聊**
 
-# training procedure
+# procedure
+1. 初始化项目(rasa init);  
+2. 准备NLU数据(data/nlu.yml);  
+3. 配置NLU模型。  
+4. 准备故事数据(data/stories.yml);  
+5. 定义领域(domain.yml);  
+6. 配置rasa core模型；  
+7. 训练模型；  
+8. 测试机器人； 
+
+# main procedure
 1. rasa data validate
 
         用法: rasa data validate [-h] [-v] [-vv] [--quiet] [-d DOMAIN] [--data DATA]
@@ -18,54 +28,23 @@
          --quiet        将日志记录级别设置为WARNING。(默认:None)
 
 2. rasa train (train both nlu and core)  
-3. rasa run -m models --endpoints endpoints.yml (run the rasa bot)  
-4. rasa shell (interactive with chat bot)
-5. rasa evaluate markers all out.csv (evaluate chat bot) 
-
-## 模块化
-Action - Rasa NLU - Rasa Core - Web Server
-
-## Rasa NLU
-使用自然语言理解进行意图识别和实体提取
-### Example:
-rquest(part)
-`"张青红的生日什么时候"`
-
-response
-```json
-{
-  "intent": "view_defendant_data",
-  "entities": {
-    "defendant" : "张青红",
-    "item" : "生日"
-  }
-}
-```
-### Pipeline
-假设我们在config文件中这样设置pipeline`"pipeline": ["Component A", "Component B", "Last Component"]`
-
-### Training
+3. rasa run actions (optional) if you redefined actions, 
+4. rasa run -m models --endpoints endpoints.yml (run the rasa bot)  
+5. rasa shell (interactive with chat bot)
+6. rasa evaluate markers all out.csv (evaluate chat bot) 
 
 
-## Rasa Core
-用于对话管理
+## TODO 
+###  Action
+1. 进行数据校验, 和数据交互，采用MySQL存储FQA数据，通过接口实现功能分离；  
+2. 采用Py2Neo与数据库(Neo4j)进行交互. 
 
-### Pipeline
-1. Rasa_Core首先接收到信息, 将信息传递给`Interpreter`, `Interpreter`将信息打包为一个字典(`dict`), 这个`dict`包括原始信息(`original text`), 意图(`intent`)的找到的所有实体(`entities`)
-2. `Tracker`保持对话的状态.
-3. `Policy` 接收到当前`Tracker`的状态
-4. `Policy`选择执行哪个动作(`Action`)
-5. 被选中的`Action`同时被`Tracker`记录
-6. `Action`执行后产生回应
-
-### Training
-
+### Tokenizer
+1. 自定义分词器，实现实体边界的覆盖；
 
 ### Interactive Learning
+1. 交互式学习，提供反馈并修正错误；
 
-在交互式学习模式下, 我们可以为Bot对话提供反馈. 这是一个非常强有力的方式去检测Bot能做什么, 同时也是修改错误最简单的方式. 基于机器学习的对话的有点就在于当bot不知道如何回答或者回答错误时, 我们可以及时的反馈给bot. 有些人称这种方式为Software 2.0
-
-
-### Action
-进行数据校验, 和数据交互. 
-采用Py2Neo与数据库(Neo4j)进行交互. 
+### Deploy
+1. 模型部署与负载均衡；
+2. 服务访问方式。
--- actions/actions.py
+++ actions/actions.py
@@ -1,60 +1,93 @@
-import logging
-import json
-from datetime import datetime
-from typing import Any, Dict, List, Text, Optional
+from typing import Any, Text, Dict, List
+from datetime import datetime, timedelta
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.types import DomainDict
-from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import (
-    SlotSet,
-    UserUtteranceReverted,
-    ConversationPaused,
-    EventType,
-)
-
-from actions import config
-from actions.api import community_events
-from actions.api.algolia import AlgoliaAPI
-from actions.api.discourse import DiscourseAPI
-from actions.api.gdrive_service import GDriveService
-from actions.api.mailchimp import MailChimpAPI
-from actions.api.rasaxapi import RasaXAPI
-
-USER_INTENT_OUT_OF_SCOPE = "out_of_scope"
-
-logger = logging.getLogger(__name__)
-
-INTENT_DESCRIPTION_MAPPING_PATH = "actions/intent_description_mapping.csv"
 
 
-class TravelSlotValidation(FormValidationAction):
-    @staticmethod
-    def location_db() -> List[Text]:
-        return ['copenhagen', 'vancouver', 'london', 'paris', 'lima', 'new york', 'milan']
+def text_date_to_int(text_date):
+    if text_date == "今天":
+        return 0
+    if text_date == "明天":
+        return 1
+    if text_date == "昨天":
+        return -1
 
-    def validate_location(
+    # in other case
+    return None
+
+
+weekday_mapping = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"]
+
+
+def weekday_to_text(weekday):
+    return weekday_mapping[weekday]
+
+
+class ActionQueryTime(Action):
+    def name(self) -> Text:
+        return "action_query_time"
+
+    def run(
         self,
-        slot_value: Any,
-        dispatcher: "CollectingDispatcher",
-        tracker: "Tracker",
-        domain: "DomainDict",
-    ) -> Dict[Text, Any]:
-        if slot_value.lower() in self.location_db():
-            return {"location": slot_value}
-        else:
-            return {"location": None}
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
-    async def extract_has_booked(
+        current_time = datetime.now().strftime("%H:%M:%S")
+        dispatcher.utter_message(text=current_time)
+
+        return []
+
+
+class ActionQueryDate(Action):
+    def name(self) -> Text:
+        return "action_query_date"
+
+    def run(
         self,
-        dispatcher: "CollectingDispatcher",
-        tracker: "Tracker",
-        domain: "DomainDict",
-    ) -> Dict[Text, Any]:
-        location = tracker.get_slot("location")
-        email_address = tracker.get_slot("email_address")
-        if location and email_address:
-            return {"has_booked": True}
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        text_date = tracker.get_slot("date") or "今天"
+
+        int_date = text_date_to_int(text_date)
+        if int_date is not None:
+            delta = timedelta(days=int_date)
+            current_date = datetime.now()
+
+            target_date = current_date + delta
+
+            dispatcher.utter_message(text=target_date.strftime("%Y-%m-%d"))
         else:
-            return {"has_booked": None}
+            dispatcher.utter_message(text="系统暂不支持'{}'的日期查询".format(text_date))
+
+        return []
+
+
+class ActionQueryWeekday(Action):
+    def name(self) -> Text:
+        return "action_query_weekday"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        text_date = tracker.get_slot("date") or "今天"
+
+        int_date = text_date_to_int(text_date)
+        if int_date is not None:
+            delta = timedelta(days=int_date)
+            current_date = datetime.now()
+
+            target_date = current_date + delta
+
+            dispatcher.utter_message(text=weekday_to_text(target_date.weekday()))
+        else:
+            dispatcher.utter_message(text="系统暂不支持'{}'的星期查询".format(text_date))
+
+        return []
